8f90f4bf6f86d3cda597029e1399e7eb82aa2021
Chrislundh98 <christoffer.lundh98@gmail.com>
2024-09-10 19:52:18 +0200

    Update test_api.py

--- tests/test_api.py
+++ tests/test_api.py
@@ -2,22 +2,63 @@ import requests
 from calculator_client.client import Client
 from calculator_client.api.actions import calculate
 from calculator_client.models.calculation import Calculation
-from calculator_client.models.opertions import Opertions
+from calculator_client.models.operations import Operations
 from calculator_client.models import ResultResponse
 
-class TestAPI():
-    def test_calc_add(self):
-        url = "http://127.0.0.1:5000/calculate"
-        payload = {
-            "operation1" : 'add' ,
-            "operand1" : 1 ,
-            "operand2" : 1 
-
-        }
-        response = requests.post(url, json=payload)
+class BaseTest:
+    def setup_method(self):
+        self.url = "http://127.0.0.1:5000/calculate"
     
+    def teardown_method(self):
+        self.url = None
+
+class TestAPI(BaseTest):
+    def test_calc_add(self):
+        payload = {
+            "operation1": 'add',
+            "operand1": 1,
+            "operand2": 2
+        }
+        response = requests.post(self.url, json=payload)
+        assert response.status_code == 200
+        result = response.json()
+        assert result['result'] == 3
+
+    def test_calc_sub(self):
+        payload = {
+            "operation1": 'sub',
+            "operand1": 1,
+            "operand2": 2
+        }
+        response = requests.post(self.url, json=payload)
+        assert response.status_code == 200
+        result = response.json()
+        assert result['result'] == -1
+
+    def test_calc_mul(self):
+        payload = {
+            "operation1": 'mul',
+            "operand1": 3,
+            "operand2": 6
+        }
+        response = requests.post(self.url, json=payload)
+        assert response.status_code == 200
+        result = response.json()
+        assert result['result'] == 18
+
+    def test_calc_div(self):
+        payload = {
+            "operation1": 'div',
+            "operand1": 6,
+            "operand2": 2
+        }
+        response = requests.post(self.url, json=payload)
+        assert response.status_code == 200
+        result = response.json()
+        assert result['result'] == 3
+
     def test_gen_calc_client(self):
-        client = Client('http://localhost:5000')
-        response = calculate.sync(client = client, body = Calculation(Opertions.ADD, operand1=1, operand2=2))
+        client = Client(base_url="http://127.0.0.1:5000")
+        response = calculate.sync(client=client, body=Calculation(operation=Operations.ADD, operand1=1, operand2=2))
         assert isinstance(response, ResultResponse)
         assert response.result == 3
